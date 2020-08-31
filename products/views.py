02ce2a8e06bc7f00e0669da259c1a2bbc2f26691
Fatima Aminu <teemamin@yahoo.com>
2020-08-31 12:44:16 +0000

    filter,sorting,search added to products view, base css updated

--- products/views.py
+++ products/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,\
+    redirect, reverse
+from django.db.models import Q
+from django.contrib import messages
 from .models import Product, Category
+from .filters import ProductFilter
 # Create your views here.
 
 
 def all_products(request):
-    products = Product.objects.all
+    products = Product.objects.all()
     categories = Category.objects.all
+    # myFilter = ProductFilter(request.GET, queryset=products)
+    # products = myFilter.qs
+    if request.GET:
+        if 'cat' in request.GET:
+            categories = request.GET['cat'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+        if 'search' in request.GET:
+            search_word = request.GET['search']
+            if not search_word:
+                messages.error(request, "please enter a message")
+                return redirect(reverse('products'))
+            search_words = Q(name__icontains=search_word) |\
+                Q(description__icontains=search_word)\
+                | Q(color__icontains=search_word)
+            products = products.filter(search_words)
     context = {
         'products': products,
-        'categories': categories
+        'categories': categories,
+        #'myFilter': myFilter
     }
     return render(request, 'products/products.html', context)
 
